6720da6497a6cfd1d4af7b3717e35d2b6870d206
marcRaes <contact@marcraes.fr>
2025-08-24 10:40:53 +0200

    feat: reduce margins to optimize print space

--- src/Service/Export/TimeSheetConfigurator.php
+++ src/Service/Export/TimeSheetConfigurator.php
@@ -11,6 +11,16 @@ readonly class TimeSheetConfigurator
     {
         $sheet->getPageSetup()
             ->setOrientation(PageSetup::ORIENTATION_LANDSCAPE)
-            ->setPaperSize(PageSetup::PAPERSIZE_A4);
+            ->setPaperSize(PageSetup::PAPERSIZE_A4)
+            ->setFitToWidth(1)
+            ->setFitToHeight(0);
+
+        $sheet->getPageMargins()
+            ->setTop(0.5)
+            ->setBottom(0.5)
+            ->setLeft(0.70)
+            ->setRight(0.70)
+            ->setHeader(0.30)
+            ->setFooter(0.30);
     }
 }
